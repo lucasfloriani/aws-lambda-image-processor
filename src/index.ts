d3504a13e953a03d6c38fc5cf065948118d0f4da
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-05-06 19:20:19 -0300

    Add suport for multiple records in a s3 put event

--- src/index.ts
+++ src/index.ts
@@ -13,31 +13,35 @@ export const handler: AWSLambda.S3Handler = async (event) => {
   const bucketName = event.Records[0].s3.bucket.name
   const destinationBucketName = process.env.OUTPUT_BUCKET || bucketName
 
-  const fileKey = formatURI(event.Records[0].s3.object.key)
-  const fileExtension = getFileExtension(fileKey)
-  const filename = getFilenameWithExtension(fileKey)
+  const recordEvents = event.Records.map(async (record) => {
+    const fileKey = formatURI(record.s3.object.key)
+    const fileExtension = getFileExtension(fileKey)
+    const filename = getFilenameWithExtension(fileKey)
 
-  if (!isAValidImageType(fileExtension)) {
-    console.error(`Not a permited image type: ${fileExtension}`)
-    return
-  }
+    if (!isAValidImageType(fileExtension)) {
+      console.error(`Not a permited image type: ${fileExtension}`)
+      return
+    }
 
-  try {
-    const originalFileInBucket = await S3.getObject({ Bucket: bucketName, Key: fileKey }).promise()
-    const processedImages = await processor(originalFileInBucket.Body as Buffer)
+    try {
+      const originalFileInBucket = await S3.getObject({ Bucket: bucketName, Key: fileKey }).promise()
+      const processedImages = await processor(originalFileInBucket.Body as Buffer)
 
-    const uploadImages = processedImages.map((imageInfo) => {
-      return S3.putObject({
-        Bucket: destinationBucketName,
-        Key: `${imageInfo.directory}/${filename}`,
-        Body: imageInfo.content,
-        ContentType: `image/${fileExtension}`,
-        ACL: 'public-read',
-      }).promise()
-    })
+      const uploadImages = processedImages.map((imageInfo) => {
+        return S3.putObject({
+          Bucket: destinationBucketName,
+          Key: `${imageInfo.directory}/${filename}`,
+          Body: imageInfo.content,
+          ContentType: `image/${fileExtension}`,
+          ACL: 'public-read',
+        }).promise()
+      })
 
-    await Promise.all(uploadImages)
-  } catch (e) {
-    console.error(e)
-  }
+      await Promise.all(uploadImages)
+    } catch (e) {
+      console.error(e)
+    }
+  })
+
+  await Promise.all(recordEvents)
 }
